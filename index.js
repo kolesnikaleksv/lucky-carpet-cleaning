8d705bb902794d1118a6061e5c7adefe7e064995
kolesnikaleksv <kolesnikaleksv@gmail.com>
2026-02-19 13:08:26 +0200

    Add favicon.ico and services block

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
 'use strict';
 
 const mobileMenuButton = document.querySelector('.menu-button');
-const body = document.querySelector('body');
+const body = document.body;
 const menu = document.querySelector('.menu');
 const menuWrapper = document.querySelector('.menu-wrapper');
 const closeBtn = document.querySelector('.close-btn');
 const menuItemsColection = document.querySelectorAll('.menu-item');
-console.log(menuItemsColection);
-
-mobileMenuButton.addEventListener('click', () => {
-  openMobileMenu();
-});
-
-closeBtn.addEventListener('click', () => {
-  if (document.body.classList.contains('isOpen')) {
-    closeMobileMenu();
-  }
-});
-
-menuItemsColection.forEach((item) => {
-  item.addEventListener('click', () => {
-    closeMobileMenu();
-  });
-});
 
 const closeMobileMenu = () => {
-  document.body.classList.remove('isOpen');
+  body.classList.remove('isOpen');
   menuWrapper.classList.remove('wrapper-menu-active');
   menu.classList.remove('mobile-menu-active');
   mobileMenuButton.classList.remove('hidden');
@@ -35,6 +18,13 @@ const openMobileMenu = () => {
   document.body.classList.add('isOpen');
   menuWrapper.classList.add('wrapper-menu-active');
   menu.classList.add('mobile-menu-active');
-
   mobileMenuButton.classList.add('hidden');
 };
+
+mobileMenuButton.addEventListener('click', openMobileMenu);
+
+closeBtn.addEventListener('click', closeMobileMenu);
+
+menuItemsColection.forEach((item) =>
+  item.addEventListener('click', closeMobileMenu),
+);
